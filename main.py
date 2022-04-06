3240db1096ba1f4699c3215260d6ec50ba7186df
AroundKiki <36398891+AroundKiki@users.noreply.github.com>
2022-04-06 17:43:55 +0800

    将UI引入主程序

--- main.py
+++ main.py
@@ -3,12 +3,22 @@
 # conf.prog.powershell = None  # Enable VBS fallback
 from scapy.all import *
 # import scapy.all as scapy
-
+from PyQt5 import QtCore, QtGui, QtWidgets
+from program_ui import *
 
 
 if __name__ == '__main__':
-    print('start')
-    show_interfaces()
-    result = sniff(iface='Intel(R) Wi-Fi 6 AX201 160MHz')
-    result.show()
+    import sys
+
+
+    # print('start')
+    # show_interfaces()
+    # result = sniff(iface='Intel(R) Wi-Fi 6 AX201 160MHz')
+    # result.show()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
